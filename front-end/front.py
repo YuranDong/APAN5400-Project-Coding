d36adabca3aad8b9364cb7568303ee6692ec43fb
Kunlang <kl.zhou2001@qq.com>
2024-04-29 01:38:23 -0400

    finish the front-end webpage

--- front-end/front.py
+++ front-end/front.py
@@ -1,40 +1,28 @@
-# import libraries
 from flask import Flask, render_template, request
-import pandas as pd
-import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import io
-import base64
-import urllib.parse
 from query import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
 # general welcome webpage with three buttons that are accessible for additional webpages
 def welcome():
-    global engine
-    engine = connect_to_postgresql()
-    # connect_to_mongodb()
     return render_template('welcome.html')
+
 
 # navigation to property webpage
 @app.route('/property', methods=['GET', 'POST'])
 def property():
     if request.method == "POST":
         property_id = int(request.form['property_id'])
-        property_data = query_property_id(property_id,engine=engine)
-        
         review_word = request.form['review_word']
-        review_data = query_property_word(review_word)
         operation = request.form['operation']
+
         if operation == 'REVIEW':
-            # return render_template('review_result.html', property=review_data.to_html(index=False))
-            # use 111111 since not available for review data
-            return "111111"
+            review_data = query_property_word(property_id, review_word, collection=collection)
+            return render_template('review_result.html', property=review_data.to_html(index=False))
         else:
+            property_data = query_property_id(property_id, engine=engine)
             return render_template('property_result.html', property=property_data.to_html(index=False))
     return render_template('property.html')
 
@@ -42,24 +30,30 @@ def property():
 # navigation to neighborhood webpage
 @app.route('/neighborhood', methods=['GET', 'POST'])
 def neighborhood():
-    neighborhoods  = get_neighborhood_list(1)
+    neighborhoods = get_neighborhood_list(engine=engine)
     if request.method == 'POST':
         selected_neighborhood = request.form['neighborhood']
-        details = get_neighborhood_details(selected_neighborhood)
-        plots = get_plots(1)
-        return render_template('neighborhood.html', neighborhoods=neighborhoods, details=details.to_html(index=False), selected_neighborhood=selected_neighborhood, plots = plots)
-    return render_template('neighborhood.html', neighborhoods=neighborhoods, details=None, plots = None)
+        details = get_neighborhood_details(selected_neighborhood, engine=engine)
+        plots = get_plots_neighbourhood(selected_neighborhood, engine=engine)
+        return render_template('neighborhood.html', neighborhoods=neighborhoods, details=details.to_html(index=False),
+                               selected_neighborhood=selected_neighborhood, plots=plots)
+    return render_template('neighborhood.html', neighborhoods=neighborhoods, details=None, plots=None)
+
 
 # navigation to city webpage
 @app.route('/city', methods=['GET', 'POST'])
 def city():
-    cities  = get_city_list(1)
+    cities = get_city_list(engine=engine)
     if request.method == 'POST':
         selected_city = request.form['city']
-        details = get_city_details(selected_city)
-        plots = get_plots(1)
-        return render_template('city.html', cities=cities, details=details.to_html(index=False), selected_city=selected_city, plots = plots)
-    return render_template('city.html', cities=cities, details=None, plots = None)
+        details = get_city_details(selected_city, engine=engine)
+        plots = get_plots_city(selected_city, engine=engine)
+        return render_template('city.html', cities=cities, details=details.to_html(index=False),
+                               selected_city=selected_city, plots=plots)
+    return render_template('city.html', cities=cities, details=None, plots=None)
+
 
 if __name__ == '__main__':
+    engine = connect_to_postgresql()
+    collection = connect_to_mongodb()
     app.run(debug=True)
